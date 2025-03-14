6397a9cc2748dfe0069352dda98bec38e792ef20
fl0jah <jonflor624@gmail.com>
2025-03-14 01:16:02 -0400

    Modified pom.xml to working SpecialSource Maven Plugin
    SpecialSource Plugin added to Maven's pom.xml
    
    Automatically handles Mojang mapping conversion to spigot on build
    
    Can use Mojang mappings in IDE
    
    Push is working

--- ExperimentPlugin/src/main/java/com/fattied/fl0jah/main/Main.java
+++ ExperimentPlugin/src/main/java/com/fattied/fl0jah/main/Main.java
@@ -1,25 +1,39 @@
 package com.fattied.fl0jah.main;
 
-import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.craftbukkit.v1_21_R3.entity.CraftPlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.mojang.authlib.GameProfile;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.server.level.ServerPlayer;
 
-import net.minecraft.server.level.EntityPlayer;
-
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin implements Listener {
 	
 	@Override
 	public void onEnable() {
-		ShapedRecipe recipe;
-		EntityPlayer player;
-		GameProfile prof;
-		// ServerPlayer pl; Mojang mapping (Cannot find)
+//		 ShapedRecipe recipe;
+//		 EntityPlayer player;
+//		 GameProfile prof;
+//		 ServerPlayer pl; Mojang mapping (Cannot find)
+		
 		System.out.println("[ExperimentPlugin] Plugin Successfully Enabled!");
+		
+		this.getServer().getPluginManager().registerEvents(this, this);
 	}
 	
 	@Override
 	public void onDisable() {
 		System.out.println("[ExperimentPlugin] Plugin Successfully Disabled!");
+	}
+	
+	@EventHandler
+	public void onSneak(PlayerToggleSneakEvent e) {
+		CraftPlayer cp = (CraftPlayer) e.getPlayer();
+		
+		cp.getHandle().connection.send();
+		
+		ServerPlayer ent;
 	}
 }
