89dadc3d393fdba1251c4c0fc37eb958fafae177
fl0jah <jonflor624@gmail.com>
2025-03-14 01:36:58 -0400

    PlayerDeath Packet Impl
    Immediately Respawn. No kill message. Maven build to target folder

--- ExperimentPlugin/src/main/java/com/fattied/fl0jah/main/Main.java
+++ ExperimentPlugin/src/main/java/com/fattied/fl0jah/main/Main.java
@@ -3,12 +3,16 @@ package com.fattied.fl0jah.main;
 import org.bukkit.craftbukkit.v1_21_R3.entity.CraftPlayer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+// import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import net.minecraft.network.protocol.Packet;
-import net.minecraft.server.level.ServerPlayer;
+// import net.minecraft.network.protocol.Packet;
+// import net.minecraft.server.level.ServerPlayer;
 
+// import net.minecraft.network.protocol.game.ClientboundAnimatePacket; // Eclipse could not find
+   import net.minecraft.network.protocol.game.ClientboundGameEventPacket;
+   
 public class Main extends JavaPlugin implements Listener {
 	
 	@Override
@@ -29,11 +33,9 @@ public class Main extends JavaPlugin implements Listener {
 	}
 	
 	@EventHandler
-	public void onSneak(PlayerToggleSneakEvent e) {
-		CraftPlayer cp = (CraftPlayer) e.getPlayer();
+	public void onDeath(PlayerDeathEvent e) {
+		CraftPlayer cp = (CraftPlayer) e.getEntity();
 		
-		cp.getHandle().connection.send();
-		
-		ServerPlayer ent;
+		cp.getHandle().connection.send(new ClientboundGameEventPacket(ClientboundGameEventPacket.IMMEDIATE_RESPAWN, 1));
 	}
 }
